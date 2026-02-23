a42c05430765954f1a487e932bc689dbf1907d7c
badhaks <haks1226@gmail.com>
2026-02-23 13:28:49 +0900

    Update stocks.js

--- api/stocks.js
+++ api/stocks.js
@@ -3,6 +3,8 @@ import { kv } from "@vercel/kv";
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
   if (req.method === "OPTIONS") return res.status(200).end();
 
   if (req.method === "GET") {
